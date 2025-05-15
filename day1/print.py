9d56e98767e8617454dcd8885730837624a25c2c
Gita Ghasemi <gita.ghasemi2003@gmail.com>
2025-05-15 23:55:59 +0330

    Add day 1 projects

---
+++ day1/print.py
@@ -0,0 +1,8 @@
+print("Day 1- ")
+print("print('what to print')")
+print("String1\nString2\nMerging Strings")
+
+# name = input("What is your name?")
+# print("Hello " + name + "!")
+
+print("Hello " + input("What's your name?") + "!")
---
+++ day1/project.py
@@ -0,0 +1,8 @@
+print("Welcome! ")
+
+city = input("Enter your city: \n")
+petName = input("Enter your pet name: \n")
+
+print("Your band name could be " + city + " " + petName)
+
+
