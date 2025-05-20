d239d2158f74310de0d1f0eb64ecea68214f5eca
Gita Ghasemi <gita.ghasemi2003@gmail.com>
2025-05-20 21:03:40 +0330

    This code is for

---
+++ day6/Reeborgs_World_Hurdle2.py
@@ -0,0 +1,31 @@
+################################################################
+# Attention: This code was written for REEBORG'S WORLD... 
+# ...Hurdle 2 challenge.
+# And will NOT run without its library. -Git;)
+################################################################
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def turn_step():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    turn_step()
+    
+#for step in range(1, 7):
+#    turn_step()
+
+################################################################
+# WARNING: Do not change this comment.
+# Library Code is below.
+################################################################
