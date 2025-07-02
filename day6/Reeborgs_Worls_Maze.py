2613f0d183349f3217c5251a0a9dedca1d608f09
Gita Ghasemi <gita.ghasemi2003@gmail.com>
2025-07-03 02:52:56 +0330

    Finishing day 6. Yohooo! ^-^

--- day6/Reeborgs_Worls_Maze.py
+++ day6/Reeborgs_Worls_Maze.py
@@ -12,26 +12,18 @@ def turn_right():
     turn_left()
     turn_left()
     turn_left()
-
-def jump():
-    turn_left()
-    while wall_on_right():
-        move()
-    turn_right()
+    
+while front_is_clear():
     move()
-    turn_right()
-    while front_is_clear():
-        move()
-    turn_left()    
-  
+turn_left()    
+    
 while not at_goal():
-    if wall_in_front():
-        jump()
-    else:
+    if right_is_clear():
+        turn_right()
         move()
-''' 
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
 
-################################################################
-# WARNING: Do not change this comment.
-# Library Code is below.
-################################################################
+'''
